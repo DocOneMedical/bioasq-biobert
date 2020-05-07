dc0723beca2aec287daa1dea62f537c726151677
Max Del Giudice <maxdelgiudice@gmail.com>
2020-05-07 20:54:30 +0000

    add colab specific script

--- docone.py
+++ docone.py
@@ -1,37 +1,48 @@
 import json
 import pandas as pd
 
+import tensorflow as tf
+
 
 NO_TOKEN = "no"
 YES_TOKEN = "yes"
 
 
-def json_to_csv(json_file: str, output_csv: str):
+def condense_files_to_csv(json_dir, output_csv):
+    files = tf.io.gfile.glob(f"{json_dir}/*.json")
+    final_dict = {}
+    for f in files:
+        update_dict(f, final_dict)
+
+    df = []
+    for (article_id, rx_code, mesh_id), related in final_dict.items():
+        df.append({
+            'article_id': article_id,
+            'rx_code': rx_code,
+            'mesh_id': mesh_id,
+            'related': related
+        })
+
+    if df:
+        pd.DataFrame(df).sort_values(['article_id', 'rx_code', 'mesh_id']) \
+                        .reindex(columns=['article_id', 'rx_code', 'mesh_id', 'related']) \
+                        .to_csv(output_csv, index=False)
+
+
+def update_dict(json_file: str, final_dict):
     with open(json_file, 'r') as jhandle:
         d = json.load(jhandle)
 
-    final_dict = {}
     for key, val in d.items():
-        print(key)
         article_id, rx_code, mesh_id = key.split('_')[:3]
         truth_val = True if val[0] == YES_TOKEN else False
         key = (article_id, rx_code, mesh_id)
-      
+     
         if truth_val:
             final_dict[key] = truth_val
         elif key not in final_dict:
             final_dict[key] = truth_val
 
-    df = []
-    for (article_id, rx_code, mesh_id), related in final_dict.items():
-        df.append({
-            'article_id': article_id, 
-            'rx_code': rx_code, 
-            'mesh_id': mesh_id, 
-            'related': related
-        })
-
-    pd.DataFrame(df).to_csv(output_csv, index=False) 
 
 if __name__ == '__main__':
-    json_to_csv('outputs/testing/predictions/predictions_0.json', 'temp.csv')
+    condense_files_to_csv('outputs/testing/predictions', 'temp.csv')
