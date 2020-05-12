83146d20918acc08c2bb2560766d55f3faa5d03f
Max Del Giudice <maxdelgiudice@gmail.com>
2020-05-12 20:19:41 +0000

    update run colab list script

--- bin/run_colab_list.sh
+++ bin/run_colab_list.sh
@@ -23,9 +23,9 @@ python run_list.py \
      --do_train=${do_train} \
      --do_predict=True \
      --vocab_file=$BIOBERT_DIR/vocab.txt \
-     --docone --docone_directory=$BIOASQ_DIR/QA/docone/various \
+     --docone --docone_directory=$BIOASQ_DIR/various \
      --bert_config_file=$BIOBERT_DIR/bert_config.json \
-     --init_checkpoint=$BIOBERT_DIR/model.ckpt-14599 \
+     --init_checkpoint=$OUTPUT_DIR/model.ckpt-1988 \
      --max_seq_length=${max_seq} \
      --train_batch_size=12 \
      --learning_rate=5e-6 \
@@ -34,4 +34,4 @@ python run_list.py \
      --do_lower_case=False \
      --train_file=$BIOASQ_DIR/BioASQ-6b/train/Full-Abstract/BioASQ-train-factoid-6b-full-annotated.json \
      --predict_file=$BIOASQ_DIR/BioASQ-6b/test/Full-Abstract/BioASQ-test-factoid-6b-3.json \
-     --output_dir=${OUTPUT_DIR} 
+     --output_dir=${OUTPUT_DIR}
