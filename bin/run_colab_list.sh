fcdb6431a24b0154b4bdd9f2048a99895c67ba6e
Max Del Giudice <maxdelgiudice@gmail.com>
2020-05-13 21:05:06 +0000

    update colab scripts

--- bin/run_colab_list.sh
+++ bin/run_colab_list.sh
@@ -1,6 +1,4 @@
 export BASE_DIR=gs://biobert_params/biobert_apr
-export BIOBERT_DIR=${BASE_DIR}/bert_factoid_list
-export BIOASQ_DIR=${BASE_DIR}/datasets
 
 while getopts ":o:t:m:d:" opt; do
   case $opt in
@@ -12,28 +10,35 @@ while getopts ":o:t:m:d:" opt; do
     ;;
     d) do_train="$OPTARG"
     ;;
+    k) dataset="$OPTARG"
+    ;;
+    c) checkpt="$OPTARG"
+    ;;
     \?) echo "Invalid option -$OPTARG" >&2
     ;;
   esac
 done
 
+export DATASET_DIR=${BASE_DIR}/datasets
+export COMMON_DIR=${BASE_DIR}/common
 export OUTPUT_DIR=${BASE_DIR}/outputs/${out}
+
 
 python run_list.py \
      --do_train=${do_train} \
      --do_predict=True \
-     --vocab_file=$BIOBERT_DIR/vocab.txt \
-     --docone --docone_directory=$BIOASQ_DIR/various \
-     --bert_config_file=$BIOBERT_DIR/bert_config.json \
-     --init_checkpoint=$BIOBERT_DIR/model.ckpt-14599 \
+     --vocab_file=${COMMON_DIR}/vocab.txt \
+     --docone --docone_directory=${DATASET_DIR}/${dataset} \
+     --bert_config_file=${COMMON_DIR}/bert_config.json \
+     --init_checkpoint=${BASE_DIR}/${checkpt} \
      --max_seq_length=${max_seq} \
      --train_batch_size=10 \
      --learning_rate=5e-6 \
      --doc_stride=128 \
      --num_train_epochs=7 \
      --do_lower_case=False \
-     --train_file=$BIOASQ_DIR/BioASQ-6b/train/Full-Abstract/BioASQ-train-list-6b-full-annotated.json \
-     --predict_file=$BIOASQ_DIR/BioASQ-6b/test/Full-Abstract/BioASQ-test-list-6b-3.json \
+     --train_file=${DATASET_DIR}/BioASQ-6b/train/Full-Abstract/BioASQ-train-list-6b-full-annotated.json \
+     --predict_file=${DATASET_DIR}/BioASQ-6b/test/Full-Abstract/BioASQ-test-list-6b-3.json \
      --output_dir=${OUTPUT_DIR}
 
 # --tpu --tpu_ip=???
--- bin/run_colab_yesno.sh
+++ bin/run_colab_yesno.sh
@@ -1,6 +1,4 @@
 export BASE_DIR=gs://biobert_params/biobert_apr
-export BIOBERT_DIR=${BASE_DIR}/bert_yesno
-export BIOASQ_DIR=${BASE_DIR}/datasets
 
 while getopts ":o:t:m:d:" opt; do
   case $opt in
@@ -12,27 +10,33 @@ while getopts ":o:t:m:d:" opt; do
     ;;
     d) do_train="$OPTARG"
     ;;
+    k) dataset="$OPTARG"
+    ;;
+    c) checkpt="$OPTARG"
+    ;;
     \?) echo "Invalid option -$OPTARG" >&2
     ;;
   esac
 done
 
+export DATASET_DIR=${BASE_DIR}/datasets
+export COMMON_DIR=${BASE_DIR}/common
 export OUTPUT_DIR=${BASE_DIR}/outputs/${out}
 
 python run_yesno.py \
     --do_train=${do_train} \
     --do_predict=True \
-    --docone --docone_directory=$BIOASQ_DIR/D000006_D001145 \
-    --vocab_file=$BIOBERT_DIR/vocab.txt \
-    --bert_config_file=$BIOBERT_DIR/bert_config.json \
-    --init_checkpoint=$BIOBERT_DIR/model.ckpt-14470 \
+    --docone --docone_directory=${DATASET_DIR}/${dataset} \
+    --vocab_file=${COMMON_DIR}/vocab.txt \
+    --bert_config_file=${COMMON_DIR}/bert_config.json \
+    --init_checkpoint=${BASE_DIR}/${checkpt} \
     --max_seq_length=${max_seq} \
     --train_batch_size=16 \
     --learning_rate=5e-6 \
     --doc_stride=128 \
     --do_lower_case=False \
     --num_train_epochs=2 \
-    --train_file=$BIOASQ_DIR/BioASQ-6b/train/Snippet-as-is/BioASQ-train-yesno-6b-snippet.json \
-    --predict_file=$BIOASQ_DIR/BioASQ-6b/test/Snippet-as-is/BioASQ-test-yesno-6b-3-snippet.json \
+    --train_file=${DATASET_DIR}/train/Snippet-as-is/BioASQ-train-yesno-6b-snippet.json \
+    --predict_file=${DATASET_DIR}/test/Snippet-as-is/BioASQ-test-yesno-6b-3-snippet.json \
     --output_dir=${OUTPUT_DIR} \
     --use_tpu --tpu_name=grpc://${tpu_ip}:8470 
