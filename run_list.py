175497a1b86180f848ec178b1c7b602335ce500e
Max Del Giudice <maxdelgiudice@gmail.com>
2020-05-14 20:33:34 +0000

    remove test break

--- run_list.py
+++ run_list.py
@@ -1343,8 +1343,6 @@ def main(_):
                                   FLAGS.do_lower_case, output_prediction_file,
                                   output_nbest_file, output_null_log_odds_file)
 
-                # TODO for testing
-                break
         else:
             eval_examples = read_squad_examples(
                     input_file=FLAGS.predict_file, is_training=False)
